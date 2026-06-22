9ab833c7f7d9f3c186b873a48accf1a6c1b53d6d
Alessio Palmero Aprosio <alessio@apnetwork.it>
2026-06-22 21:03:35 +0000

    Update PublicationsPage with new conference presentations and links

--- frontend/src/views/PublicationsPage.vue
+++ frontend/src/views/PublicationsPage.vue
@@ -2,37 +2,44 @@
   <div class="container mt-4">
     <h1>Publications</h1>
     <h2>Conference presentations</h2>
-<ul>
-<li>
-  Principe, C., Marini, C., Aprosio, A. (2025).
-  “Linguistic analyses of Volcanic Activity”. Oral presentation at the International Conference <em>The Awakening of Enceladus. A Transdisciplinary Inquiry into Ecological Risk and Disaster.</em>
-  Pisa (Italy), 17-19 December 2025.
-  <a href="https://www.fondazionereturn.it/wp-content/uploads/2025/11/BOOK-OF-ABSTRACTS-ENG.pdf">[Link]</a>
-</li>
-<li>
-  Principe, C., Marini, C. (2025).
-  “The use of historical sources in the assessment of volcanic scenarios and risk management”. PICO presentation at the NH2.2 session <em>Preventive volcanic risk management. Convergences of institutional role players and scientific knowledge of the European Geophysical Union General Assembly 2025 (EGU25).</em>
-  Vienna (Austria), 27 April-2 May 2025.
-  <a href="https://doi.org/10.5194/egusphere-egu25-21244">[Link]</a>
-</li>
-<li>
-  Principe, C., Marini, C. (2024).
-  “Come possono i documenti del passato aiutarci a comprendere l’attività vulcanica futura?” Oral presentation at the Conference of the Italian Society of History of Science (SISS) <em>Le radici del futuro. Tradizioni, innovazioni e sfide in storia della scienza.</em>
-  Bari (Italy), 29-31 May 2024.
-  <a href="https://www.societastoriadellascienza.it/images/convegni-siss/Bari-2024/Book_of_Abstract_definitivo.pdf">[Link]</a>
-</li>
-<li>
-  Principe, C., Marini, C. (2023).
-  “Using text corpora for volcanic eruption impact assessment and resilience planning: The first steps of the CorVo project”. Oral presentation at the NH2.2 session <em>Preventive volcanic risk management. Convergences of institutional role players and scientific knowledge of the European Geophysical Union General Assembly 2023 (EGU23).</em>
-  Vienna (Austria), 24-28 April 2023.
-  <a href="https://doi.org/10.5194/egusphere-egu23-15120">[Link]</a>
-</li>
-<li>
-  Principe, C., Marini, C. (2023).
-  “Using text corpora for volcanic eruption forecasting, impact assessment and resilience planning: The first steps of the CorVo project”. Poster presentation at the <em>IAVCEI Scientific Assembly 2023.</em>
-  Rotorua (New Zealand), 30 January-3 February 2023.
-  <a href="https://www.iavceivolcano.org/content/uploads/2021/03/iavcei-2023-book-of-abstracts.pdf">[Link]</a>
-</li>
-</ul>
+    <ul>
+      <li>
+        Marini, C., Casagrande G., Principe, C., Aprosio, A. (2026).
+        “Extracting Volcanological Knowledge from Historical Texts: A Language-Technology Pipeline for Diachronic Geovisualization”.
+        Oral presentation at the <em>Workshop on Language Technologies for Historical and Ancient Languages (LT4HALA).</em>
+        Palma (Spain), 11 May 2026.
+        <a href="http://lrec-conf.org/proceedings/lrec2026/workshops/lt4hala/2026.lt4hala-1.0.pdf">[Link]</a>
+      </li>
+      <li>
+        Principe, C., Marini, C., Aprosio, A. (2025).
+        “Linguistic analyses of Volcanic Activity”. Oral presentation at the International Conference <em>The Awakening of Enceladus. A Transdisciplinary Inquiry into Ecological Risk and Disaster.</em>
+        Pisa (Italy), 17-19 December 2025.
+        <a href="https://www.fondazionereturn.it/wp-content/uploads/2025/11/BOOK-OF-ABSTRACTS-ENG.pdf">[Link]</a>
+      </li>
+      <li>
+        Principe, C., Marini, C. (2025).
+        “The use of historical sources in the assessment of volcanic scenarios and risk management”. PICO presentation at the NH2.2 session <em>Preventive volcanic risk management. Convergences of institutional role players and scientific knowledge of the European Geophysical Union General Assembly 2025 (EGU25).</em>
+        Vienna (Austria), 27 April-2 May 2025.
+        <a href="https://doi.org/10.5194/egusphere-egu25-21244">[Link]</a>
+      </li>
+      <li>
+        Principe, C., Marini, C. (2024).
+        “Come possono i documenti del passato aiutarci a comprendere l’attività vulcanica futura?” Oral presentation at the Conference of the Italian Society of History of Science (SISS) <em>Le radici del futuro. Tradizioni, innovazioni e sfide in storia della scienza.</em>
+        Bari (Italy), 29-31 May 2024.
+        <a href="https://www.societastoriadellascienza.it/images/convegni-siss/Bari-2024/Book_of_Abstract_definitivo.pdf">[Link]</a>
+      </li>
+      <li>
+        Principe, C., Marini, C. (2023).
+        “Using text corpora for volcanic eruption impact assessment and resilience planning: The first steps of the CorVo project”. Oral presentation at the NH2.2 session <em>Preventive volcanic risk management. Convergences of institutional role players and scientific knowledge of the European Geophysical Union General Assembly 2023 (EGU23).</em>
+        Vienna (Austria), 24-28 April 2023.
+        <a href="https://doi.org/10.5194/egusphere-egu23-15120">[Link]</a>
+      </li>
+      <li>
+        Principe, C., Marini, C. (2023).
+        “Using text corpora for volcanic eruption forecasting, impact assessment and resilience planning: The first steps of the CorVo project”. Poster presentation at the <em>IAVCEI Scientific Assembly 2023.</em>
+        Rotorua (New Zealand), 30 January-3 February 2023.
+        <a href="https://www.iavceivolcano.org/content/uploads/2021/03/iavcei-2023-book-of-abstracts.pdf">[Link]</a>
+      </li>
+    </ul>
   </div>
 </template>
